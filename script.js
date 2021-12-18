820d47f57221dc45a391a4e6505ec921da30f15f
willianbarata <willianbarata@gmail.com>
2021-12-18 14:42:25 -0200

    relacionando html com js, div em variáveis

---
+++ script.js
@@ -0,0 +1,16 @@
+let order = [];
+
+let clickedOrder = [];
+
+let score = 0;
+
+//0 = verde 
+//1 = vermelho 
+//2 = amarelo
+//3 = azul
+
+const blue = document.querySelector('.blue');
+const red = document.querySelector('.red');
+const green = document.querySelector('.green');
+const yellow = document.querySelector('.yellow');
+
