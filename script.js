9bd1b511accee9173ac3247d64e6beb381d133a9
willianbarata <willianbarata@gmail.com>
2021-12-18 16:46:52 -0200

    Projeto concluído, incluí tempo para piscar

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ const red = document.querySelector('.red');
 const green = document.querySelector('.green');
 const yellow = document.querySelector('.yellow');
 
+green.onclick = () => click(0);
+red.onclick = () => click(1);
+yellow.onclick = () => click(2);
+blue.onclick = () => click(3);
 
 //order aleatória
 let shuffleOrder = () => {
@@ -29,6 +33,8 @@ let shuffleOrder = () => {
         let elementColor = createColorElement(order[i]);
         lightColor(elementColor, Number(i) + 1);
     }
+
+    
 }
 
 //próxima cor
@@ -36,7 +42,10 @@ let lightColor = (element, number) => {
     number = number * 500;
     setTimeout(() => {
         element.classList.add('selected');
-    }, number - 250);
+        setTimeout(()=> {
+            element.classList.remove('selected');
+        }, number - 200);
+    }, number - 350);
     setTimeout(() => {
         element.classList.remove('selected');
     });
@@ -44,6 +53,7 @@ let lightColor = (element, number) => {
 
 //botões são os mesmo da ordem do jogo
 let checkOrder = () => {
+    
     for(let i in clickedOrder){
         if(clickedOrder[i] != order[i]){
             lose();
@@ -60,12 +70,55 @@ let checkOrder = () => {
 
 //função do click
 let click = (color) => {
+    
     clickedOrder[clickedOrder.length] = color;
     createColorElement(color).classList.add('selected');
 
     setTimeout(() => {
         createColorElement(color).classList.remove('selected');
-    });
+        checkOrder();
+    }, 250);
 
-    checkOrder();
+    
 }
+
+//função que retorna a cor
+let createColorElement = (color) => {
+    if(color == 0){
+        return green;
+    }else if(color == 1){
+        return red;
+    }else if(color == 2){
+        return yellow;
+    }else if(color == 3){
+        return blue;
+    }
+}
+
+//função para o próximo nível do jogo
+let nextLevel = () =>{
+    score++;
+    shuffleOrder();
+}
+
+//função jogador perdedor
+let lose = () =>{
+    alert(`Pontuação: ${score} \n
+          Você perdeu o jogo!! \n
+          Clique em ok para iniciar um novo jogo`);
+    order = [];
+    clickedOrder = [];
+
+    inicio();
+}
+
+let inicio = () => {
+    alert(`Bem vindo ao Gênesis! Iníciando novo Jogo`);
+    score = 0;
+
+    nextLevel();
+}
+
+
+
+inicio();
